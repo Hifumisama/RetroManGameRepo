fbbbb4c8cc35052fd93085a181fef1db4e10d6b7
Hifumisama <m.khayat92@gmail.com>
2017-05-21 00:40:31 +0200

    Ajout du formulaire de contact, il est opérationnel en localhost. Il ne reste plus qu'a lui attribuer la fonction de mailing...

--- RetroManGameSymfony/src/RMGBundle/Controller/DefaultController.php
+++ RetroManGameSymfony/src/RMGBundle/Controller/DefaultController.php
@@ -2,7 +2,17 @@
 
 namespace RMGBundle\Controller;
 
+// ici se trouve le chemin de notre entité contact :D
+use RMGBundle\Entity\contact;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
@@ -39,9 +49,52 @@ class DefaultController extends Controller
     {
         return $this->render('RMGBundle:Site:reservation.html.twig');
     }
-    public function contactAction()
+    public function contactAction(Request $request)
     {
-        return $this->render('RMGBundle:Site:contact.html.twig');
+        // on instancie notre entité contact
+        $contact = new contact();
+
+        //on instancie le formulaire via le service form factory
+        $formbuilder = $this->get("form.factory")->createBuilder(Formtype::class,$contact);
+
+        // on remplit notre objet formulaire avec les champs que l'on veut
+        $formbuilder
+        ->add("nom", TextType::class)
+        ->add("prenom", TextType::class)
+        ->add("adresse", TextType::class)
+        ->add("email", TextType::class)
+        ->add("sujet", TextareaType::class)
+        ->add("Annuler", ResetType::class)
+        ->add("Envoyer", SubmitType::class)
+        ;
+
+        // on s'occupe maintenant de générer le formulaire avec les champs ainsi fournis
+        $form = $formbuilder->getForm();
+
+        // à ce moment on doit aussi vérifier la pertinence des informations que l'utilisateur nous as fournies
+
+        /* Etape 1 on vérifie que la méthode est bien un POST*/
+
+        if($request->isMethod("POST")) {
+          // si c'est bon on récupère les infos de l'utilisateur dans l'objet contact
+          //grâce à la variable form qui nous as permis de créer le formulaire
+
+          if($form->handleRequest($request)->isValid()) {
+            // ces commandes servent à enregistrer les données dans la base
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($contact);
+            $em->flush();
+
+            // un petit message pour l'utilisateur en tant que feedback
+            return $this->render('RMGBundle:Site:index.html.twig');
+          }
+
+        }
+
+        // on fait passer ce magnifique formulaire à notre vue
+        return $this->render('RMGBundle:Site:contact.html.twig', array (
+          'form' => $form->createView(),
+        ));
     }
     public function webmapAction()
     {
@@ -51,5 +104,5 @@ class DefaultController extends Controller
     {
         return $this->render('RMGBundle:Site:legal.html.twig');
     }
-    
+
 }
---
+++ RetroManGameSymfony/src/RMGBundle/Entity/contact.php
@@ -0,0 +1,250 @@
+<?php
+
+namespace RMGBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * contact
+ *
+ * @ORM\Table(name="contact")
+ * @ORM\Entity(repositoryClass="RMGBundle\Repository\contactRepository")
+ */
+class contact
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     * @Assert\length(
+     *       min=2,
+     *       max = 40,
+     *       minMessage = "Votre nom est trop court, il doit être d'au moins {{ limit }} caractères",
+     *       maxMessage = "Votre nom est trop long, il doit être au maximum de {{ limit }} caractères"
+     * )
+     *
+     */
+    private $nom;
+
+    /**
+     * @var string
+     * @ORM\Column(name="prenom", type="string", length=255)
+     * @Assert\length(
+     *       min= 2,
+     *       max = 40,
+     *       minMessage = "Votre prenom est trop court, il doit être d'au moins {{ limit }} caractères",
+     *       maxMessage = "Votre prenom est trop long, il doit être au maximum de {{ limit }} caractères"
+     * )
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     * @Assert\length(
+     *       min = 10,
+     *       max = 150,
+     *       minMessage = "Votre adresse est trop courte, elle doit être d'au moins {{ limit }} caractères",
+     *       maxMessage = "Votre adresse est trop longue, elle doit être au maximum de {{ limit }} caractères"
+     * )
+     * @ORM\Column(name="adresse", type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     * @Assert\Email(
+     *     message = "L'Email '{{ value }}' est incorrecte",
+     * )
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sujet", type="text")
+     * @Assert\length(
+     *       min = 10,
+     *       max = 1000,
+     *       minMessage = "Votre texte est trop court, il doit être d'au moins {{ limit }} caractères",
+     *       maxMessage = "Votre texte est trop long, il doit être au maximum de {{ limit }} caractères"
+     * )
+     */
+    private $sujet;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="datecreation", type="date")
+     */
+    private $datecreation;
+
+    public function __construct() {
+      $this->datecreation = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return contact
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set prenom
+     *
+     * @param string $prenom
+     *
+     * @return contact
+     */
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get prenom
+     *
+     * @return string
+     */
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set adresse
+     *
+     * @param string $adresse
+     *
+     * @return contact
+     */
+    public function setAdresse($adresse)
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    /**
+     * Get adresse
+     *
+     * @return string
+     */
+    public function getAdresse()
+    {
+        return $this->adresse;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return contact
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set sujet
+     *
+     * @param string $sujet
+     *
+     * @return contact
+     */
+    public function setSujet($sujet)
+    {
+        $this->sujet = $sujet;
+
+        return $this;
+    }
+
+    /**
+     * Get sujet
+     *
+     * @return string
+     */
+    public function getSujet()
+    {
+        return $this->sujet;
+    }
+
+    /**
+     * Set datecreation
+     *
+     * @param \DateTime $datecreation
+     *
+     * @return contact
+     */
+    public function setDatecreation($datecreation)
+    {
+        $this->datecreation = $datecreation;
+
+        return $this;
+    }
+
+    /**
+     * Get datecreation
+     *
+     * @return \DateTime
+     */
+    public function getDatecreation()
+    {
+        return $this->datecreation;
+    }
+}
