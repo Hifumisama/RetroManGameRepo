178f8ec3b47f25208d337a35179eddac561a9a37
Quentin Vilcoque <q.vilcoque@gmail.com>
2017-06-15 07:59:28 +0200

    reservation plus tard
    - creation de l'entité salle
    - maj de la bdd

---
+++ RetroManGameSymfony/src/RMGBundle/Entity/salle.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace RMGBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * salle
+ *
+ * @ORM\Table(name="salle")
+ * @ORM\Entity(repositoryClass="RMGBundle\Repository\salleRepository")
+ */
+class salle
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="type", type="string", length=255)
+     */
+    private $type;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="datedeb", type="datetime")
+     */
+    private $datedeb;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="datefin", type="datetime")
+     */
+    private $datefin;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set type
+     *
+     * @param string $type
+     *
+     * @return salle
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set datedeb
+     *
+     * @param \DateTime $datedeb
+     *
+     * @return salle
+     */
+    public function setDatedeb($datedeb)
+    {
+        $this->datedeb = $datedeb;
+
+        return $this;
+    }
+
+    /**
+     * Get datedeb
+     *
+     * @return \DateTime
+     */
+    public function getDatedeb()
+    {
+        return $this->datedeb;
+    }
+
+    /**
+     * Set datefin
+     *
+     * @param \DateTime $datefin
+     *
+     * @return salle
+     */
+    public function setDatefin($datefin)
+    {
+        $this->datefin = $datefin;
+
+        return $this;
+    }
+
+    /**
+     * Get datefin
+     *
+     * @return \DateTime
+     */
+    public function getDatefin()
+    {
+        return $this->datefin;
+    }
+}
+
